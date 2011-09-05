6b53b92ff99bd37ea9b5cf3217dfa5a4b6d1e12f
Brian Woodward <brian.woodward@gmail.com>
2011-09-05 02:30:20 -0400

    blah

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
+  before_filter :signedin_user, :only => [:new, :create]
   
   def index
     @title = "All users"
@@ -49,8 +50,13 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
+    @user = User.find(params[:id])
+    if (current_user?(@user))
+      flash[:error] = "You cannot destroy yourself!"
+    else
+      @user.destroy
+      flash[:success] = "User destroyed."
+    end
     redirect_to users_path
   end
 
@@ -67,5 +73,9 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def signedin_user
+      redirect_to(root_path) if signed_in?
     end
 end
